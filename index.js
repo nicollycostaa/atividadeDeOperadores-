f2c6bd632c1dd4e412b4dff9d1af2102c2fbc958
nicollycostaa <92127366+nicollycostaa@users.noreply.github.com>
2022-04-13 14:28:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const idade = 17;
+
+if(idade>=18){
+    console.log("Você pode entrar")
+}else{
+    console.log("Você não pode entrar por não ter a idade necessária!")
+} if(idade == 17){
+    console.log("Volte quando você tiver 18!")
+}
+
+
+
+
+
